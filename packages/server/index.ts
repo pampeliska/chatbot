15f829363e3a9dce948cf877590d3931e1f73de6
Jana Šnirychová <jana.snirychova@gmail.com>
2025-11-19 19:39:06 +0100

    api key env

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,15 @@
+import dotenv from 'dotenv';
 import express, { type Request, type Response } from 'express';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log('BOTAI_API_KEY:', process.env.BOTAI_API_KEY);
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World');
+  res.send('test');
 });
 
 app.listen(port, () => {
