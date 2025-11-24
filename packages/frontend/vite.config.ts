d787cb66c136300a6b7c5abe32aec6c765b354c1
Jana Šnirychová <jana.snirychova@gmail.com>
2025-11-24 16:15:51 +0100

    add tailwindcss

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+
   server: {
     proxy: {
       '/api': 'http://localhost:3000', // Proxy API requests to the backend server /api/test -> http://localhost:3000/api/test
