d3f70130cafa1372b9426f68c071a9c331da15de
Jana Šnirychová <jana.snirychova@gmail.com>
2025-11-24 17:11:45 +0100

    add ui.shadcn

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,7 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000', // Proxy API requests to the backend server /api/test -> http://localhost:3000/api/test
