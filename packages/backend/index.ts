d5557dbee8e70d26cb616aecb9dbe7ceedbe3ad1
Jana Šnirychová <jana.snirychova@gmail.com>
2026-04-12 16:18:50 +0200

    add error response for status code 500

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -46,19 +46,24 @@ app.post('/api/chat', async (req: Request, res: Response) => {
       return;
    }
 
-   const { prompt, conversationId } = req.body;
+   try {
+      const { prompt, conversationId } = req.body;
+      const response = await client.responses.create({
+         model: 'gpt-4o-minir',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         previous_response_id: converstations.get(conversationId),
+      });
 
-   const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      previous_response_id: converstations.get(conversationId),
-   });
+      converstations.set(conversationId, response.id);
 
-   converstations.set(conversationId, response.id);
-
-   res.json({ message: response.output_text });
+      res.json({ message: response.output_text });
+   } catch (error) {
+      res.status(500).json({
+         error: 'Something went wrong. Failed to generate response.',
+      });
+   }
 });
 
 app.listen(port, () => {
