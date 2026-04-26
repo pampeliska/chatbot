98443b018bd2933483fb8e7392dd0afeb22239d7
Jana Šnirychová <jana.snirychova@gmail.com>
2026-04-26 20:12:11 +0200

    form design

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,19 +1,17 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/ui/button';
+import ChatBot from './components/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
+   // const [message, setMessage] = useState('');
 
-   useEffect(() => {
-      fetch('/api/test')
-         .then((response) => response.json())
-         .then((data) => setMessage(data.message));
-   }, []);
+   // useEffect(() => {
+   //    fetch('/api/test')
+   //       .then((response) => response.json())
+   //       .then((data) => setMessage(data.message));
+   // }, []);
 
    return (
       <div className="p-4">
-         <p className="font-bold text-3xl p-7">{message}</p>
-         <Button variant="secondary">Subscribe</Button>
+         <ChatBot />
       </div>
    );
 }
